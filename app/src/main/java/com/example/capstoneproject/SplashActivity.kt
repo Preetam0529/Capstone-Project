4d5fe520dc05ca9671e22322a630f3be7b64333b
Preetam Das <preetam2429@gmail.com>
2025-03-22 00:45:56 +0530

    initial commit

---
+++ app/src/main/java/com/example/capstoneproject/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.capstoneproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+    }
+
+}
